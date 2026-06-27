b88a468fd11e00cd17c6aa1dc1ba0370fcf89cf4
Sergey Ryabin <ryabin.ss@gmail.com>
2026-06-28 00:35:36 +0400

    fix team lead #3

--- src/openhands_langgraph/__init__.py
+++ src/openhands_langgraph/__init__.py
@@ -2,7 +2,11 @@ from __future__ import annotations
 
 from .graph import build_development_graph, build_single_role_graph, build_team_lead_graph
 from .nodes import OpenHandsLangGraphError, run_openhands_role_node
+from .role_policy import install_runtime_policy_hooks
 from .state import OpenHandsGraphState
+from .team_lead import DirectLLMTeamLeadRunner, TeamLeadDecision, TeamLeadDecisionResult
+
+install_runtime_policy_hooks()
 
 __all__ = [
     "OpenHandsGraphState",
@@ -15,5 +19,3 @@ __all__ = [
     "TeamLeadDecision",
     "TeamLeadDecisionResult",
 ]
-
-from .team_lead import DirectLLMTeamLeadRunner, TeamLeadDecision, TeamLeadDecisionResult
